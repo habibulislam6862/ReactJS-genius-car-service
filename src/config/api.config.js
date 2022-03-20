186c17fbb7f467c72ecdd70713ac87d89b8e0821
Habibul Islam <habibulislam6862@gmail.com>
2022-03-21 02:57:41 +0600

    liveserver added;

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,3 +1,3 @@
-export const port = 8000;
-const apiDomain = `http://localhost:${port}`;
+// export const port = 8000;
+const apiDomain = `https://polar-fortress-26028.herokuapp.com`;
 export default apiDomain;
