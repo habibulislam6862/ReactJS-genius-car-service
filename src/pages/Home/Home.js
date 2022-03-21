e9e3a64e51e05476d5686cde98942aebb370a5ae
Habibul Islam <habibulislam6862@gmail.com>
2022-03-21 19:01:07 +0600

    title fixed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Carousel from './Carousel/Carousel';
 import Services from './Services/Services';
 
 const Home = () => {
+    useEffect(() => {
+        document.title = 'Genius Car Service';
+    }, []);
     return (
         <div>
             <Carousel/>
