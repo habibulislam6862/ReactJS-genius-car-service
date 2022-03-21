73a6136a87336c6ca0232156b0313731f1d52f04
Habibul Islam <habibulislam6862@gmail.com>
2022-03-21 19:05:16 +0600

    home page service card link fixed

--- src/pages/Home/Card/Card.js
+++ src/pages/Home/Card/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Card.module.css';
 
-const Card = ({ service, max }) => {
-    const { picture, service: name, price, description, id } = service;
+const Card = ({ service }) => {
+    const { picture, service: name, price, description, _id } = service;
     return (
         <div className='col-md-3'>
             <div className='p-3 d-flex flex-column rounded  bg-dark h-100'>
@@ -17,7 +17,7 @@ const Card = ({ service, max }) => {
                     <h3><span className='text-danger'>Only </span><span className='text-light'>${price}</span></h3>
                     <div className='text-light'>{description}</div>
                     <div className="d-grid mt-3">
-                        <NavLink to={`/blog/${id}`} className='btn btn-block btn-light'>Read more</NavLink>
+                        <NavLink to={`/blog/${_id}`} className='btn btn-block btn-light'>Read more</NavLink>
                     </div>
                     
                 </div>
